40e632ee57b1edd443a83becbac8365f3f9de642
Jule <34949343+sparks-fly-archive@users.noreply.github.com>
2018-04-30 17:57:38 +0200

    Update snorre.py

--- snorre.py
+++ snorre.py
@@ -1,9 +1,23 @@
 import discord
 import mysql.connector
+from mysql.connector import errorcode
 import os
 
 client = discord.Client()
 
+# connect to database
+
+try:
+cnx = mysql.connector.connect(user=os.getenv('USER'), password=os.getenv('PASS'),
+                              host=os.getenv('HOST'),
+                              database=os.getenv('DATABASE'))
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
 
 @client.event
 async def on_message(message):
