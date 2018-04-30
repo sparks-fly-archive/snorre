54e79a5eb393969d843224e86eaf4936ea3107cd
Jule <34949343+sparks-fly-archive@users.noreply.github.com>
2018-04-30 17:04:55 +0200

    Create snorre.py

---
+++ snorre.py
@@ -0,0 +1,24 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run('NDQwNTE0MjQzNzY3NTY2Mzg3.Dci1yg.GSe6dldz25nb8MGjr6UB3Hsbi1o')
